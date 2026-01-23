9adfc0180f28626b02f02a84160acb2382bb8435
Ram Poptani <911S205126@adani.com>
2026-01-23 13:35:27 +0530

    INIT

---
+++ src/main/java/org/binance/webconsumer/WebConsumerApplication.java
@@ -0,0 +1,13 @@
+package org.binance.webconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/binance/webconsumer/WebConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.binance.webconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
