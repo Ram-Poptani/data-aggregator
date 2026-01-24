7554e9d939ee1c86aca352249d63338d01d2b45a
Ram Poptani <911S205126@adani.com>
2026-01-24 16:37:13 +0530

    chore: Add JsonMapper bean to Jackson configuration

--- src/main/java/org/binance/webconsumer/config/JacksonConfig.java
+++ src/main/java/org/binance/webconsumer/config/JacksonConfig.java
@@ -3,6 +3,7 @@ package org.binance.webconsumer.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class JacksonConfig {
@@ -10,5 +11,10 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public JsonMapper jsonMapper() {
+        return new JsonMapper();
     }
 }
