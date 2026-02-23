50467ae23892ef3314cc8f77ab89bf17fa6f4b8f
Ram Poptani <911S205126@adani.com>
2026-02-23 12:50:19 +0530

    chore: remove unused ObjectMapper dependency and configuration from JacksonConfig

--- src/main/java/org/binance/webconsumer/config/JacksonConfig.java
+++ src/main/java/org/binance/webconsumer/config/JacksonConfig.java
@@ -1,17 +1,11 @@
 package org.binance.webconsumer.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class JacksonConfig {
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
 
     @Bean
     public JsonMapper jsonMapper() {
