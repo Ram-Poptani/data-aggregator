17812bdbbfcd9d50d39a10694227d592a56cef27
Ram Poptani <911S205126@adani.com>
2026-02-25 22:02:23 +0530

    feat: Enhance RabbitMQService with metrics for message publishing

--- src/main/java/org/binance/webconsumer/services/RabbitMQService.java
+++ src/main/java/org/binance/webconsumer/services/RabbitMQService.java
@@ -2,7 +2,12 @@ package org.binance.webconsumer.services;
 
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import lombok.Getter;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class RabbitMQService {
 
     private final RabbitTemplate rabbitTemplate;
@@ -20,14 +24,38 @@ public class RabbitMQService {
     @Value("${rabbit-mq.default.exchange}")
     private String defaultExchange;
 
+//    Metrics
+    private final Counter tradesSentCounter;
+    private final Counter publishErrorCounter;
+    private final Timer publishTimer;
+
+    @Getter
+    private volatile Instant lastSentTime;
+
+    public RabbitMQService(
+                            RabbitTemplate rabbitTemplate,
+                            MeterRegistry meterRegistry
+                            ) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.tradesSentCounter = meterRegistry.counter("rabbitmq.messages.trades.sent");
+        this.publishErrorCounter = meterRegistry.counter("rabbitmq.publish.errors");
+        this.publishTimer = meterRegistry.timer("rabbitmq.messages.publish.time");
+
+    }
+
     public void sendMessage(String exchange, String routingKey, Serializable message) {
         try {
             log.debug("Sending message to RabbitMQ - Exchange: {}, Routing Key: {}", exchange, routingKey);
-            rabbitTemplate.convertAndSend(exchange, routingKey, message);
+            publishTimer.record(() -> {
+                rabbitTemplate.convertAndSend(exchange, routingKey, message);
+            });
             log.debug("Message sent successfully to exchange: {}, routingKey: {}", exchange, routingKey);
+            this.tradesSentCounter.increment();
+            this.lastSentTime = Instant.now();
         } catch (Exception e) {
             // TODO: Catch specific exceptions related to RabbitMQ and add retry mechanism if needed
             log.error("Failed to send message to RabbitMQ - Exchange: {}, Routing Key: {}", exchange, routingKey, e);
+            this.publishErrorCounter.increment();
             throw new RuntimeException("Failed to send message to RabbitMQ", e);
         }
     }
@@ -35,16 +63,4 @@ public class RabbitMQService {
     public void sendMessage(String routingKey, Serializable message) {
         this.sendMessage(defaultExchange, routingKey, message);
     }
-
-    public void broadcastMessage(Serializable message) {
-        try {
-            log.debug("Broadcasting message to FanoutExchange: {}", defaultExchange);
-            rabbitTemplate.convertAndSend(defaultExchange, "", message);
-            log.debug("Message broadcast successfully");
-        } catch (Exception e) {
-            log.error("Failed to broadcast message", e);
-            throw new RuntimeException("Failed to broadcast message", e);
-        }
-    }
-
 }
